dd58de01a89940ba263d65751259332a4f2ec764
Barry Wardell <barry.wardell@gmail.com>
2021-05-31 22:53:55 +0100

    Create radial grid as part of the full calculation

--- generate_source.py
+++ generate_source.py
@@ -7,27 +7,31 @@ import shutil
 
 def main():
     # Parse arguments
-    if len(sys.argv) != 7:
-        print('Usage:', sys.argv[0], '<r0> <lmaxret> <lmaxS> <lmax2> <grid> <h1P>')
+    if len(sys.argv) != 6:
+        print('Usage:', sys.argv[0], '<r0> <lmaxret> <lmaxS> <lmax2> <h1P>')
         sys.exit(1)
-    (r0, lmaxret, lmaxS, lmax2, grid, h1P) = sys.argv[1:]
+    (r0, lmaxret, lmaxS, lmax2, h1P) = sys.argv[1:]
     
-    # Check radial grid and h1P files exist
-    if not os.path.isfile(grid):
-        print('Radial grid file', grid, 'does not exist.')
-        sys.exit(1)
+    # Check h1P file exists
 
     if not os.path.isfile(h1P):
         print('h1P file', h1P, 'does not exist.')
         sys.exit(1)
     
     # Create output directories
+    griddir = 'data/grids'
     h1dir = 'data/h1/r0_'+r0
     h1retdir = h1dir + '/no-ddh/h1ret'
     evenstaticdir = h1dir + '/EvenStatic/h1ret'
     d2Rdir = 'data/d2R/r0_'+r0
     os.makedirs(h1retdir)
     os.makedirs(evenstaticdir)
+    os.makedirs(griddir)
+
+    # Create radial grid
+    grid = griddir + '/radial_grid_r'+r0+'.h5'
+    mathcode = 'r0=' + r0 + '; gridFile="' + grid + '"; Get["h1Lorenz/notebooks/ComputeRadialGrid.wl"]; Quit[];'
+    subprocess.run(['wolfram', '-run', mathcode])
 
     # Run h1Lorenz
     subprocess.run(['mpirun', '-n', '2', './h1Lorenz/h1Lorenz', r0, lmaxret, grid, h1retdir])
